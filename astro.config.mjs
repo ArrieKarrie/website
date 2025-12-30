47d60d704ea530829594f46d24059b6dab77884f
ArrieKarrie <armanvandijk@gmail.com>
2025-12-30 13:07:24 +0100

    Configure custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: 'https://ArrieKarrie.github.io',
-  base: '/website/',
+  site: 'www.armanvandijk.nl',
+  base: '/',
 });
